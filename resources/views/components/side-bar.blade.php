6cdf89dba945eb1a206f1ff8410ed57dd5c84e1c
Herly Riyanto Hidayat <herlyryanth@gmail.com>
2026-02-01 10:58:44 +0800

    refactor(sidebar): improve UI/UX with sticky layout and custom scrollbar

--- resources/views/components/side-bar.blade.php
+++ resources/views/components/side-bar.blade.php
@@ -123,7 +123,7 @@
                                         d="M4 6H2v14c0 1.1.9 2 2 2h14v-2H4V6zm16-4H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-1 9H9V9h10v2zm-4 4H9v-2h6v2zm4-8H9V5h10v2z" />
                                 </svg>
                             </div>
-                            <span class="font-medium">Master Data General</span>
+                            <span class="font-medium">Master General</span>
                         </div>
                         <svg class="w-4 h-4 transition-transform duration-200" :class="open ? 'rotate-180' : ''"
                             fill="currentColor" viewBox="0 0 24 24">
@@ -190,7 +190,7 @@
                                         d="M19 7h-3V6a4 4 0 0 0-8 0v1H5a1 1 0 0 0-1 1v11a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V8a1 1 0 0 0-1-1z" />
                                 </svg>
                             </div>
-                            <span class="font-medium">Master Data Stok</span>
+                            <span class="font-medium">Master Stok</span>
                         </div>
                         <svg class="w-4 h-4 transition-transform duration-200" :class="open ? 'rotate-180' : ''"
                             fill="currentColor" viewBox="0 0 24 24">
